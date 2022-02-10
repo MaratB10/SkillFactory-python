35eba7e81ce691a32841867aec17ff3bb7a86a3c
MaratB10 <bmt1986@yandex.ru>
2022-02-11 01:35:02 +0300

    new

---
+++ 36/New.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def create_medications(names, counts):
+    """
+    Напишите функцию create_mediactions(names, counts), которая  создает Series medications,
+    индексами которой являются названия лекарств names, а значениями - их количество в поставке counts
+    """
+    medications = pd.Series(
+        data = names,
+        index = counts,
+        name = "medications"
+    )
+    
+    return(medications)
+   
+
+def get_percent(medications, name):
+    """
+    А также напишите функцию get_percent(medications, name), которая возвращает долю количества товара
+    с именем name от общего количества товаров в поставке в процентах.
+    """
+    return(medications[name]/sum(medications) * 100)
+
+if __name__ == '__main__':
+    names=['chlorhexidine', 'cyntomycin', 'afobazol']
+    counts=[15, 18, 7]
+    medications = create_medications(names, counts)
+    #print(get_percent(medications, "chlorhexidine")) #37.5
+    #medications["chlorhexidine"]
